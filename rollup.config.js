11d556db931d69adc230017b5c5fe668fd03d476
Mark Dornian <mark@ibexconsulting.com>
2020-09-03 16:12:29 -0700

    Rollup Fixes (WIP)

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,39 @@
+import buble from "@rollup/plugin-buble";
+import {version} from "./package.json";
+
+const banner =
+    "/*!\n" +
+    " * vue-authenticate v" + version + "\n" +
+    " * https://github.com/mdornian/vue-authenticate\n" +
+    " * Released under the MIT License.\n" +
+    " */\n";
+
 export default {
   input: 'src/index.js',
-  output: {
-    file: 'dist/vue-authenticate.js',
-    format: 'cjs',
-    exports: 'auto'
-  }
+  output: [
+    {
+      banner: banner,
+      file: 'dist/vue-authenticate.js',
+      format: 'umd',
+      name: 'VueAuthenticate'
+    },
+    {
+      banner: banner,
+      file: 'dist/vue-authenticate.min.js',
+      format: 'iife'
+    },
+    {
+      banner: banner,
+      file: 'dist/vue-authenticate.es2015.js',
+      footer: 'export { VueAuthenticate };',
+      format: 'es',
+    },
+    {
+      banner: banner,
+      exports: 'auto',
+      file: 'dist/vue-authenticate.common.js',
+      format: 'cjs'
+    }
+  ],
+  plugins: [ buble() ]
 };
